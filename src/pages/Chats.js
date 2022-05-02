55fec6be2757ac67dae6782f5c77095d8a41a16c
Ante Maric <antemaric99@hotmail.com>
2022-05-02 14:55:28 +0200

    added cards
    added cards to the chat window to make the hello world look more pleasing

--- src/pages/Chats.js
+++ src/pages/Chats.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 
 function Chats() {
     return (
-        <div>
-            <h1>Hello World! The implementation of the chat will be displayed here!</h1>
+        <div className="chatWindow">
+            <Card>
+                <Card.Header>
+                    <h1>Chat</h1>
+                </Card.Header>
+                <Card.Body>
+                    <p>Hello World! The implementation of the chat will be displayed here!</p>
+                </Card.Body>
+            </Card>
         </div>
     );
 }
