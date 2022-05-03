59f908de21c6b895369ee7f8305986268132e634
Ante Maric <antemaric99@hotmail.com>
2022-05-03 22:46:50 +0200

    added comments

--- src/pages/Chats.js
+++ src/pages/Chats.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 
+// Author: Ante Maric - 1273904
+// Implemented a basic hello world feature that will be displayed on the chat tab
 function Chats() {
     return (
         <div className="chatWindow">
