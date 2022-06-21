7cb820f0fa159b085c8b1a2f7fc913c9a260cd29
JoseCarioca <65904569+JoseCarioca@users.noreply.github.com>
2022-06-21 18:32:10 +0200

    displayname again
    to make it work I saved the username from db to displayName again, this is surely just for the session and not permanent

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -10,43 +10,51 @@ const db = getDatabase();
 const auth = getAuth()
 
 
-const student = auth.currentUser;
+
+
 var studentName;
-//PLEASE DONT CHANGE THIS CODE UNTIL I UNDERSTAND WHY IT WORKS
-function MyName(){
-  if (student !== null){
+
+function MyName(student){
+  if (student.displayName == null){ //if displayname is not already saved or was erased in the session
+    // then ask again for it in db
     return onValue(ref(db, '/users/' + student.uid + '/Username'), (snapshot) => {
-        studentName = (snapshot.val()) || 'Anonymous'; //.Username
-        console.log("snapshot is: ")
-        console.log(studentName)
+        studentName = (snapshot.val()) 
+        //console.log("snapshot is: ")
+        //console.log(studentName)
+        student.displayName = (snapshot.val());
       }, {
         onlyOnce: true
       });
-    }
+    }else console.log("already saved")
     
 }
 
 
 function MyProfile() {
+    console.log('start of reload')
     const userAuth = useUserAuth()
-    
-    MyName();
+    const student = userAuth.user.auth.currentUser
 
-    console.log('userAuth is:');
-    console.log(userAuth);
-    console.log('username is: ' + studentName); //after some time this is also null omg
-    console.log('email is: ' + userAuth.user.email);
-    console.log('uid is: '+ userAuth.user.uid);
-    //console.log('STUDENT username is: ' + student.displayName); //this is null in the object dont know why
-    console.log('STUDENT email is: ' + student.email);
-    console.log('STUDENT uid is: '+ student.uid);
+    
+    MyName(student);
+    console.log("studentname is pls:");
+    console.log(studentName);
+
+    //console.log('userAuth is:');
+    //console.log(userAuth);
+    //console.log('username is: ' + studentName); //after some time this is also null omg
+    //console.log('email is: ' + userAuth.user.email);
+    //console.log('uid is: '+ userAuth.user.uid);
+    console.log('STUDENT username is: ' + student.displayName); //this is null in the object dont know why
+    //console.log('STUDENT email is: ' + student.email);
+    //console.log('STUDENT uid is: '+ student.uid);
     
 
     return (
     <div>
         <h1>Mein Profil</h1>
         <Card>
-             <h2 className="text-center mb-4">Username: {studentName}</h2>
+             <h2 className="text-center mb-4">Username: {student.displayName}</h2>
              <h2 className="text-center mb-4">Email: {student.email}</h2>
              <h2 className="text-center mb-4">UID: {student.uid}</h2>
         </Card>
