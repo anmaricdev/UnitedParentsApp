5c6731e3956c216be2fbbc3b3dbe1f2d063fd88c
Ante Maric <49611582+anmaricdev@users.noreply.github.com>
2022-07-10 00:22:47 +0200

    changed button design
    function to create new div still not working properly

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {card} from "react-bootstrap"
+import {Button} from "react-bootstrap"
 
 //TODO: figure out how to make the cards not float above header while scrolling down
 
@@ -17,6 +17,13 @@ function Home() {
 
     <input type="text" name="motivationHeader" id="motivationHeader" placeholder="Enter a new quote..."></input>
     */
+    function createCards(){
+        var card = document.createElement("div");
+        card.innerHTML = "<h1>Testing out if it works</h1>";
+        
+        var element = document.getElementById("here");
+      element.appendChild(card);
+      }
 
     return (
         <div className="adminBoxes">
@@ -31,13 +38,14 @@ function Home() {
                     </h4>
                 </footer>
             </blockquote>
-            <button className="eButton" id="editButton">Edit</button>
-            <button type="submit" className="sButton" id="saveButton">Save</button>
+            <Button className="eButton" id="editButton">Edit</Button>
+            <Button type="submit" className="sButton" id="saveButton">Save</Button>
+            <Button className="dButton" id="deleteButton">Delete</Button>
         </div>
         
         <br></br>
 
-                <div class="card">
+                <div className="card" id="cardAdmin">
                     <img class="mx-auto" src="./pictures/fra-uas-logo.png" alt="" width="200" length="100" position="text-center"></img>
                     <div class="card-body">
                         <h1 class="card-title">Osterferienbetreuung 2022</h1>
@@ -51,31 +59,17 @@ function Home() {
                     <div class="card-footer">
                         <small class="text-muted">Last updated 3 mins ago</small>
                     </div>
-                    <button className="eButton" id="editButton">Edit</button>
-                    <button type="submit" className="sButton" id="saveButton">Save</button>
+                    <Button className="eButton" id="editButton">Edit</Button>
+                    <Button type="submit" className="sButton" id="saveButton">Save</Button>
+                    <Button className="dButton" id="deleteButton">Delete</Button>
                 </div>
 
                 <br></br>
-
-                <div class="card">
-                    <img class="mx-auto" src="./pictures/fra-uas-logo.png" alt="" width="200" length="100" position="text-center"></img>
-                    <div class="card-body">
-                        <h1 class="card-title">Osterferienbetreuung 2022</h1>
-                        <p class="card-text">vom 11.4.-22.4. an der Frankfurt University of Applied Science für Kinder im
-                            Alter von 6 bis 12. Unter dem Motto: „Frankfurt durch die Kamera“ werden wir
-                            spannende Ausflüge machen, Frankfurt durch die Kamera in den Blick nehmen, einen
-                            Animationsfilm drehen und gemeinsam viel Spaß haben. Betreuungs-Zeitraum ist
-                            täglich von 08:30 Uhr bis 17:00 Uhr. Anmeldeschluss ist der 18.03.2022! Infos zu
-                            Programm und Anmeldung <u>hier</u>.</p>
+                    <div id='here'>
+                        <Button className="cButton" id="createBtn" /*onclick={</div>createCards()}*/>create new card</Button>
                     </div>
-                    <div class="card-footer">
-                        <small class="text-muted">Last updated 3 mins ago</small>
-                    </div>
-                    <button className="eButton" id="editButton">Edit</button>
-                    <button type="submit" className="sButton" id="saveButton">Save</button>
-                </div>
-
                 <br></br>
+
             </div>
     );
 }
