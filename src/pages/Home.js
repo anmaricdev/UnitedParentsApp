ad6e4239d7e142cd69238f8096ca1177b455c1cc
Ante Maric <49611582+anmaricdev@users.noreply.github.com>
2022-07-10 16:48:51 +0200

    bug fixes
    fixed a bug where new cards couldn't be edited

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,17 +8,21 @@ import { useState } from "react";
 function Home() {
 
    function editContentBox(){
-        document.getElementById("cardHeader").contentEditable = true;
-        document.getElementById("cardBody").contentEditable = true;
-        document.getElementById("quoteOfTheDay").contentEditable = true;
-        document.getElementById("authorQuote").contentEditable = true;
+        document.getElementById("cardHeader").setAttribute('contenteditable', 'true')
+        document.getElementById("cardBody").setAttribute('contenteditable', 'true')
+        document.getElementById("quoteOfTheDay").setAttribute('contenteditable', 'true')
+        document.getElementById("authorQuote").setAttribute('contenteditable', 'true')
+        document.getElementById("newHeader").setAttribute('contenteditable', 'true')
+        document.getElementById("newBody").setAttribute('contenteditable', 'true')
     }
     
     function saveContentBox(){
-        document.getElementById("cardHeader").contentEditable = false;
-        document.getElementById("cardBody").contentEditable = false;
-        document.getElementById("quoteOfTheDay").contentEditable = false;
-        document.getElementById("authorQuote").contentEditable = false;
+        document.getElementById("cardHeader").setAttribute('contenteditable', 'false')
+        document.getElementById("cardBody").setAttribute('contenteditable', 'false')
+        document.getElementById("quoteOfTheDay").setAttribute('contenteditable', 'false')
+        document.getElementById("authorQuote").setAttribute('contenteditable', 'false')
+        document.getElementById("newHeader").setAttribute('contenteditable', 'false')
+        document.getElementById("newBody").setAttribute('contenteditable', 'false')
     }
 
     const [{cards}, setCards] = useState({cards: []});
@@ -26,13 +30,8 @@ function Home() {
         cards.push(<div key={cards.length}><div className="card" id="cardAdmin">
         <img class="mx-auto" src="./pictures/fra-uas-logo.png" alt="" width="200" length="100" position="text-center"></img>
         <div class="card-body">
-            <h1 class="card-title" id="cardHeader">Osterferienbetreuung 2022</h1>
-            <p class="card-text" id="cardBody">vom 11.4.-22.4. an der Frankfurt University of Applied Science für Kinder im
-                Alter von 6 bis 12. Unter dem Motto: „Frankfurt durch die Kamera“ werden wir
-                spannende Ausflüge machen, Frankfurt durch die Kamera in den Blick nehmen, einen
-                Animationsfilm drehen und gemeinsam viel Spaß haben. Betreuungs-Zeitraum ist
-                täglich von 08:30 Uhr bis 17:00 Uhr. Anmeldeschluss ist der 18.03.2022! Infos zu
-                Programm und Anmeldung <u>hier</u>.</p>
+            <h1 class="card-title" id="newHeader">Geben Sie hier eine Überschrift ein</h1>
+            <p class="card-text" id="newBody">Geben Sie hier Ihren Text ein</p>
         </div>
         <div class="card-footer">
             <small class="text-muted">Last updated 3 mins ago</small>
